d1cb07f532107c16300e44430548dbff235e22ba
User <66434592+khanglamthe@users.noreply.github.com>
2023-08-14 21:06:01 +0700

    update config build

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=postgres.ckabfqce1ic3.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=postgressql
+export POSTGRES_HOST=postgres.ccdtqliog0xr.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=my-288576499820-bucket
+export AWS_BUCKET=bucket-khanglt1
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
